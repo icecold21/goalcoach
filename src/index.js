6c1fff4846764444d2c44d9359124e10f49e250f
Jonathan <jonathan.wylliem@jurnal.id>
2017-07-18 21:07:09 +0700

    Fix React Router Error

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import MainApp from './components/App';
+import { Router, Route, Switch } from 'react-router';
+import createBrowserHistory from 'history/createBrowserHistory';
+
+import MainApp from './components/MainApp';
 import SignIn from './components/SignIn';
 import SignUp from './components/SignUp';
 
+const history = createBrowserHistory();
+
 ReactDOM.render(
-  <div>App</div>, document.getElementById('root')
+  <Router path="/" history={ history }>
+    <Switch>
+      <Route path="/app" component={MainApp} />
+      <Route path="/signin" component={ SignIn } />
+      <Route path="/signup" component={ SignUp } />
+    </Switch>
+  </Router>, document.getElementById('root')
 );
+
